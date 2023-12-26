9498e34f9460a1bfc86540026ca8dce898c76e36
Dejavu-Jamaisvu <122339479+Dejavu-Jamaisvu@users.noreply.github.com>
2023-12-27 00:45:26 +0900

    1번끝!

---
+++ step_by_step/1.입출력과사칙연산/1-12.고양이/10171.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    //\이것만 \\이걸로 바꿔주면 됨!
+
+    cout <<"\\    /\\" << endl;
+    cout <<" )  ( ')" << endl;
+    cout <<"(  /  )" << endl;
+    cout <<" \\(__)|" << endl;
+
+}
---
+++ step_by_step/1.입출력과사칙연산/1-13.개/10172.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    // \이거랑 "만 이스케이프 해주면 됨!
+
+    cout <<"|\\_/|" << endl;
+    cout <<"|q p|   /}" << endl;
+    cout <<"( 0 )\"\"\"\\" << endl;
+    cout <<"|\"^\"`    |" << endl;
+    cout <<"||_/=\\\\__|" << endl;
+
+}
