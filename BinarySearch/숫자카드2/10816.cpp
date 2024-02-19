0f7d00b6138bfc0eee12c2a5b242b59966dde93c
Dejavu-Jamaisvu <122339479+Dejavu-Jamaisvu@users.noreply.github.com>
2024-02-19 11:27:59 +0900

    binarysearch 주석

---
+++ BinarySearch/숫자카드2/10816.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
+//바다 - auto stl 사용
+//유민 - 이분탐색은 아니지만 풀이방법 다시 보면 좋을듯
+//유정 - 백터를 쓰면 0으로 초기화됨
