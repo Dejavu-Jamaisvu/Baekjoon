7c0ff81dca1bc701ab8d75cd8406b83db907aca9
Dejavu-Jamaisvu <122339479+Dejavu-Jamaisvu@users.noreply.github.com>
2024-02-19 14:47:51 +0900

    셋팅테스트

---
+++ test_setting/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    cout << "hello";
+
+    int n;
+    cin >> n;
+    cout << n * n;
+    
+    return 0;
+}
