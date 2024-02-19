08bb33ea8d9f5186bf7293d52bceef99aa950847
Dejavu-Jamaisvu <122339479+Dejavu-Jamaisvu@users.noreply.github.com>
2024-02-19 16:34:47 +0900

    피보나치수2

--- DynamicProgramming/피보나치수2/2748.cpp
+++ DynamicProgramming/피보나치수2/2748.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int fibonacci(int n)
-{
-    if (n == 0)
-    {
-        return 0;
-    }
-    else if (n == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
+// int fibonacci(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 0;
+//     }
+//     else if (n == 1)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+// }
 
 int main()
 {
@@ -30,7 +31,26 @@ int main()
 
     cin >> n;
 
-    cout << fibonacci(n);
+    // vector<int> arr;
+    long long arr[n]; //n이 90일 때 2880067194370816120이 나올 수 있어야 함.
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 0)
+        {
+            arr[i] = 0;
+        }
+        else if (i == 1)
+        {
+            arr[i] = 1;
+        }
+        else
+        {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+    }
+
+    cout << arr[n] << "\n";
 
     return 0;
 }
