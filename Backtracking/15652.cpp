e5233b62424bf8cde0825f2c363db8749369c9bd
REVE <aiwork4242@gmail.com>
2024-05-03 17:35:52 +0900

    다음에

---
+++ Backtracking/15652.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void Print(){
+
+    
+}
+void Choose(int curr_num, int cnt){
+
+    if(curr_num == n+1){
+        if(cnt == m)
+            Print();
+        return;
+    }
+
+}
+
+int main()
+{
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Choose(1,0);
+    
+
+    return 0;
+}
